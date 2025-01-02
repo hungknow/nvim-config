cb3dc82a69d6e2df91bad525d9f7c8ce33ef0a21
hungknow <hungbuiknow@gmail.com>
2025-01-02 17:57:17 +0700

    Support detect terraform file

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -8,6 +8,15 @@ function M:init()
 
   -- vim.cmd([[colorscheme visual_studio_code]])
   require('vscode').load('dark')
+
+  -- neovim has very simple detection logic with .tf files to 
+  -- distinguish between tinyfugue (tf) files and terraform files.
+  vim.filetype.add({
+    extension = {
+      tf = "terraform"
+    }
+  })
+
   --
   -- require("hvim.keymappings"):load_defaults()
   -- require("hvim.keymappings"):load(hvim.keys)
