c8c0ea53618ce2762aa3ce4d9bd57a175abc127c
hungknow <hungbuiknow@gmail.com>
2025-01-03 11:59:34 +0700

    Ctrl-i to display intellisense dialog

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -202,7 +202,7 @@ return {
 					["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
 					["<down>"] = cmp.mapping.select_next_item(cmp_select),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-i>"] = cmp.mapping.complete(),
 					-- ["<C-f>"] = cmp_action.luasnip_jump_forward(),
 					-- ["<C-b>"] = cmp_action.luasnip_jump_backward(),
 					-- ["<Tab>"] = cmp_action.luasnip_supertab(),
--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,65 +1,69 @@
 local M = {
-  'nvim-telescope/telescope.nvim',
-  branch = "0.1.x",
-  --lazy = true,
-  dependencies = { 
-    'nvim-lua/plenary.nvim',
-  },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	--lazy = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 }
 
 function M.init()
-  require("plugins.telescope.keymaps")
+	require("plugins.telescope.keymaps")
 end
 
 function M.config()
-  require("plugins.telescope.cmds")
-  local actions = require("utils.modules").require_on_exported_call "telescope.actions"
-  local telescope = require("telescope")
+	require("plugins.telescope.cmds")
+	local actions = require("utils.modules").require_on_exported_call("telescope.actions")
+	local telescope = require("telescope")
 
-  telescope.setup{
-    pickers = {
-      find_files = {
-        hidden = true,
-      },
-      live_grep = {
-        --@usage don't include the filename in the search results
-        only_sort_text = true,
-      },
-      grep_string = {
-        only_sort_text = true,
-      },
-      buffers = {
-        initial_mode = "normal",
-        mappings = {
-          i = {
-            ["<C-d>"] = actions.delete_buffer,
-          },
-          n = {
-            ["dd"] = actions.delete_buffer,
-          },
-        },
-      },
-      planets = {
-        show_pluto = true,
-        show_moon = true,
-      },
-      git_files = {
-        hidden = true,
-        show_untracked = true,
-      },
-      colorscheme = {
-        enable_preview = true,
-      },
-    },
-    defaults = {
-        file_ignore_patterns = {
-          ".git/", "node_modules/", "vendor/", 
-          "%.po", ".next/", "dist/", "%.d.ts",
-          "*.lock"
-        },
-    }
-  }
-
+	telescope.setup({
+		pickers = {
+			find_files = {
+				hidden = true,
+			},
+			live_grep = {
+				--@usage don't include the filename in the search results
+				only_sort_text = true,
+			},
+			grep_string = {
+				only_sort_text = true,
+			},
+			buffers = {
+				initial_mode = "normal",
+				mappings = {
+					i = {
+						["<C-d>"] = actions.delete_buffer,
+					},
+					n = {
+						["dd"] = actions.delete_buffer,
+					},
+				},
+			},
+			planets = {
+				show_pluto = true,
+				show_moon = true,
+			},
+			git_files = {
+				hidden = true,
+				show_untracked = true,
+			},
+			colorscheme = {
+				enable_preview = true,
+			},
+		},
+		defaults = {
+			file_ignore_patterns = {
+				".git/",
+				"node_modules/",
+				"vendor/",
+				"%.po",
+				".next/",
+				"dist/",
+				"%.d.ts",
+				"*.lock",
+			},
+		},
+	})
 end
 
 return M
